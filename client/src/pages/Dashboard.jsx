429dde91db4375ff767809f283e275639adaf2d6
luxxor <benjaminbeccari.1@gmail.com>
2024-12-16 19:22:46 -0300

    Componente de Dashboard actualizado

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -22,7 +22,7 @@ export const Dashboard = () => {
             setLoading(false);
         }
     };
-
+    
 
     const currentMonth = new Date().getMonth();
 
@@ -44,22 +44,21 @@ export const Dashboard = () => {
             <div className='flex h-screen'>
                 <NavBar />
                 <div className="flex-1 p-6 bg-gray-100">
-                    <h1 className='text-2xl font-bold mf-4'>Dashboard</h1>
-                    <section className='h-2/5  bg-gray-800 rounded-md'>
+                    {/* <h1 className='text-2xl font-bold mf-4'>Dashboard</h1> */}
+                    <section className='h-3/5  bg-gray-800 rounded-md'>
                         <h3 className='p-3 text-xl text-white'>Notificaciones</h3>
-
                     </section>
                     <div className='flex h-2/5'>
-                        <section className='w-2/4 my-4 mx-2 bg-gray-800 rounded-md'>
+                        <section className='w-2/4 my-4 mr-2 bg-gray-800 rounded-md'>
                             <h3 className='p-3 text-xl text-white'>Licencias Pendientes</h3>
-
                         </section>
-                        <section className='w-2/4 my-4 mx-2 bg-gray-800 rounded-md'>
+                        <section className='w-2/4 my-4 bg-gray-800 rounded-md overflow-y-auto'>
                             <h3 className='p-3 text-xl text-white'>Nuevos Ingresos</h3>
                             {filterEmployees.map((employee) => (
-                                <div key={employee._id} className='px-2 flex text-white'>
+                                <div key={employee._id} className='m-2 px-3 py-4 flex border border-slate-500 text-white rounded-md '>
                                     <h4 className='basis-1/2'>{employee.name}</h4>
-                                    <h4 className='basis-1/2'>{employee.position}</h4>
+                                    <p className='basis-1/2'>{employee.position}</p>
+                                    
                                 </div>
                             ))}
                         </section>
