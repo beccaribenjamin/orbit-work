4dff7c492fe5e5d4c7f73aae9165948f523488a2
Luxorion <benjaminbeccari.1@gmail.com>
2025-08-28 16:18:02 -0300

    fix: cambios en cors para aceptar solicitudes desde vercel

--- src/server/server.js
+++ src/server/server.js
@@ -50,7 +50,7 @@ class Server {
 
     middlewares() {
         this.app.use(cors({
-            origin: 'http://localhost:5173',
+            origin: ['http://localhost:5173', 'https://orbit-work.vercel.app'],
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
             credentials: true
         }));
