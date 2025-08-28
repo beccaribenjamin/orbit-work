03e320936b433ff18da508a6ce51e9a24b6c6cfd
Luxorion <benjaminbeccari.1@gmail.com>
2025-08-28 17:32:59 -0300

    Problemas en cors

--- src/server/server.js
+++ src/server/server.js
@@ -50,7 +50,7 @@ class Server {
 
     middlewares() {
         this.app.use(cors({
-            origin: ['http://localhost:5173', 'https://orbit-work.vercel.app/'],
+            origin: ['http://localhost:5173', 'https://orbit-work.vercel.app'],
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
             credentials: true
         }));
